ac4eebb87bf69296546be5e2ef94e11808dcaec4
_dream <will.ferens@gmail.com>
2020-10-07 22:03:20 -0600

    fixed prism

--- src/pages/about.js
+++ src/pages/about.js
@@ -28,7 +28,7 @@ export default function About() {
                         What they failed to realize is that I see development and software as an ideal outlet for my skill set. 
                     </p>
                     <p>
-                        Writing a function is just like crafting a convincing sentence. Each word has to be evaluated for effectiveness - too many and your statement becomes muddled; too few and you'll never get your point across. And just like an essay, there are a magnitude of ways to craft a program. Structure matters. Every reference to another part of the whole must be purposeful and serve the reader or user.
+                        Writing a function is just like crafting a convincing sentence. Each word has to be evaluated for effectiveness - too many and your statement becomes muddled; too few and you'll never get your point across. Brick by brick, you're building towards a cohesive end. And just like an essay, there are a magnitude of ways to craft a program. Structure matters. Every reference to another part of the whole must be purposeful and serve the reader or user.
                     </p>
                     <p>
                         Editing is refactoring. Your first draft and quick prototype will always need another critical look. Syntax might differ in English to Spanish, as from JavaScript to Java, but utilized correctly, you can create depth where you otherwise may not. Research, whether on the cutting edge of a new technology or in the dusty stacks of old academic journals, shares a thread. And, of course, there is always trial and error. While a red lettered terminal message may be more literal than an editor's pen, the process of investigation into what, why, and where things went wrong remain similar.
