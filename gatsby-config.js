4f29957a70f279d7b18bf3104fbc2b3cb3fe4ee8
_dream <will.ferens@gmail.com>
2020-10-19 22:20:09 -0600

    good to go google sheets

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,24 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-source-google-spreadsheets',
+      options: {
+        spreadsheetId: '1_PMOOq-QVWWTVN-ZUUUkYEhkwYcHqfsCspOYEXZ6coA',
+        apiKey: process.env.GOOGLE_API_KEY,
+        credentials: {
+          type: 'service_account',
+          project_id: process.env.PROJECT_ID,
+          private_key_id: process.env.PRIVATE_KEY_ID,
+          private_key: process.env.PRIVATE_KEY.replace(/(\\r)|(\\n)/g, '\n'),
+          client_email: process.env.CLIENT_EMAIL,
+          auth_uri: "https://accounts.google.com/o/oauth2/auth",
+          token_uri: "https://oauth2.googleapis.com/token",
+          auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+          client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.PROJECT_ID}%40appspot.gserviceaccount.com`
+        }
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
